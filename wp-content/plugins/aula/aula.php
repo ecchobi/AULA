52863c1c8f458460f6244f9e20b0aa0640ec7eb1
Bianca <bibias10@gmail.com>
2018-11-20 16:43:54 +0000

    calculadora

---
+++ wp-content/plugins/aula/aula.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+* Plugin Name: Aula 
+* Description: plugin
+* Version: 1.0
+* Author: Bianca R
+*/
+
+//principal função usada no plugin
+function faula( $atts ){
+    return '<iframe src="'.plugins_url().'/aula/index.php" frameborder="0" width="400px" height="500px"></iframe>';
+}
+add_shortcode('aula', 'faula');
+?>
